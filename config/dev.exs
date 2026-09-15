1873b0185a9a970a12cd73e0fde85f362356f86d
Alvaro Lizama <soy@alvarolizama.com>
2026-09-15 17:03:25 -0600

    fix(config): quita el modificador E inválido de los patrones de live_reload

--- config/dev.exs
+++ config/dev.exs
@@ -58,12 +58,12 @@ config :tokengate, TokengateWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/tokengate_web/router\.ex$"E,
-      ~r"lib/tokengate_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/tokengate_web/router\.ex$",
+      ~r"lib/tokengate_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
